96417973454146e43eadb748ebe967c3a1f6c0ce
fatbrain96 <srivastavparakhkumar@gmail.com>
2025-09-03 19:08:20 +0530

    Added ATM Project

---
+++ ATM/ATM/src/bankaccount.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class bankaccount {
+    static Scanner sc = new Scanner(System.in);
+    static void register() {
+        System.out.println("***************************");
+        System.out.println("Enter your full name :");
+        ATM.name = sc.nextLine();
+
+        System.out.println("Enter username :");
+        String username = sc.nextLine();
+
+        System.out.println("Enter your password :");
+        String password = sc.nextLine();
+
+
+        System.out.println("Enter your password :");
+        sc.nextLine();
+
+        System.out.println("Enter your Account number :");
+        ATM.account_number = sc.nextLine();
+        
+        System.out.println("REGISTRATION SUCCESSFULLY!");
+        System.out.println("***************************");
+        ATM.select_operation();
+    }
+}
